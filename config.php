d81b500d6659bb6a8777c1aafec3e1d585647f41
CupIvan <mail@cupivan.ru>
2011-07-23 18:47:08 +0400

    парсинг номера серии в сериале вида s2e07

--- config.php
+++ config.php
@@ -10,23 +10,30 @@ $oAuth_token = array(
 /** пользовательская обработка имени файла */
 function parseFileName($fname)
 {
-	// сериал "Интерны"
-	if (match($fname, '(интерн|intern).+?(\d+)', $m))
+	$res = array();
+	// какой-либо сериал
+	if (match($fname, 's(\d+)e(\d+)', $m))
 	{
-		$episode = $m[2];
-		return array(
-			'series_id'      => 1003752,
-			'season_number'  => floor(($episode - 1) / 20) + 1,
-			'episode_number' => ($episode - 1) % 20 + 1,
-		);
+		$res['season_number']  = $m[1];
+		$res['episode_number'] = $m[2];
+	}
+	// сериал "Интерны"
+	if (match($fname, '(интерн|interny).+?(\d+)?', $m))
+	{
+		$res['series_id'] = 1003752;
+		if (isset($m[2]))
+		{
+			$episode = $m[2];
+			$res['season_number']  = floor(($episode - 1) / 20) + 1;
+			$res['episode_number'] = ($episode - 1) % 20 + 1;
+		}
 	}
 	// сериал "Зайцев + 1"
-	if (match($fname, '(zaicev.+1).+?(\d+)', $m))
+	if (match($fname, '(zaicev|зайцев).{3,}(\d+)?', $m))
 	{
-		return array(
-			'series_id'      => 1642278,
-			'season_number'  => 1,
-			'episode_number' => $m[2],
-		);
+		$res['series_id']      = 1642278;
+		$res['season_number']  = 1;
+		$res['episode_number'] = $m[2];
 	}
+	return $res;
 }
