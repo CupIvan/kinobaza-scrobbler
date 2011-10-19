e97cb7b4591e3492dac952f0989b6c661c82a523
CupIvan <mail@cupivan.ru>
2011-10-19 16:11:53 +0400

    добавил сериал Dexter

--- config.php
+++ config.php
@@ -11,28 +11,33 @@ $oAuth_token = array(
 function parseFileName($fname)
 {
 	$res = array();
-	// какой-либо сериал
-	if (match($fname, 's(\d+)e(\d+)', $m))
-	{
-		$res['season_number']  = $m[1];
-		$res['episode_number'] = $m[2];
-	}
 	// сериал "Интерны"
-	if (match($fname, '(интерн|interny).+?(\d+)?', $m))
+	if (match($fname, 'интерн|interny', $m))
 	{
 		$res['series_id'] = 1003752;
-		if (isset($m[2]))
-		{
-			$episode = $m[2];
-			$res['season_number']  = floor(($episode - 1) / 20) + 1;
-			$res['episode_number'] = ($episode - 1) % 20 + 1;
-		}
+	}
+	if (match($fname, '(интерн|interny).+\D(0?\d+)', $m))
+	{
+		$episode = $m[2];
+		$res['season_number']  = floor(($episode - 1) / 20) + 1;
+		$res['episode_number'] = ($episode - 1) % 20 + 1;
 	}
 	// сериал "Зайцев + 1"
 	if (match($fname, '(zaicev|зайцев).{3,}(\d+)?', $m))
 	{
 		$res['series_id']      = 1642278;
 		$res['season_number']  = 1;
+		$res['episode_number'] = $m[2];
+	}
+	// сериал "Dexter"
+	if (match($fname, 'dexter', $m))
+	{
+		$res['series_id'] = 3446;
+	}
+	// какой-либо сериал
+	if (match($fname, 's(\d+)e(\d+)', $m))
+	{
+		$res['season_number']  = $m[1];
 		$res['episode_number'] = $m[2];
 	}
 	return $res;
